008073a7387156e06c0eade92944e6682bda9cc3
Fortune <nachodev369@gmail.com>
2023-05-18 13:01:42 -0700

    Task 0. Preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
