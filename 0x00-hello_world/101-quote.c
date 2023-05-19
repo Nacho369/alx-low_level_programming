029703fbf0fb68e8f776e4a59bae081ea0a3de84
Fortune <nachodev369@gmail.com>
2023-05-18 17:49:28 -0700

    Task 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
-
+/**
+ * main - Entry point program
+ *
+ * Description: Prints a string to the standard error
+ * followed by a new line
+ *
+ * Return: 1 (Failure)
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
