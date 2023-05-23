ee68db0bb9cffa7f943e356027acb82161e47b85
Fortune <nachodev369@gmail.com>
2023-05-23 16:40:06 -0700

    Task 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_nums(int prod, int f_d, int s_d, int l_d);
+void print_seperator(int iter, int prod);
 
 /**
  * print_times_table - Prints the n times table starting
@@ -33,37 +35,65 @@ void print_times_table(int n)
 					f_dig = tens / 10;
 				}
 
-				if (iter != 0)
-				{
-					_putchar(44);
-					_putchar(32);
+				print_seperator(iter, prod);
 
-					if (prod < 10)
-					{
-						_putchar(32);
-						_putchar(32);
-					}
-					else if (prod > 9 && prod < 100)
-						_putchar(32);
-
-				}
-
-				if (prod < 10)
-					_putchar(48 + prod);
-				else if (prod > 9 && prod < 100)
-				{
-					_putchar(48 + f_dig);
-					_putchar(48 + l_dig);
-				}
-				else
-				{
-					_putchar(48 + f_dig);
-					_putchar(48 + m_dig);
-					_putchar(48 + l_dig);
-				}
+				print_nums(prod, f_dig, m_dig, l_dig);
 			}
 
 			_putchar(10);
 		}
+	}
+}
+
+/**
+ * print_nums - Prints the digit
+ *
+ * @prod: Products of loop digits
+ * @f_d: First digit character
+ * @m_d: Middle digit character
+ * @l_d: Last digit character
+ *
+ * Return: void
+ */
+void print_nums(int prod, int f_d, int m_d, int l_d)
+{
+	if (prod < 10)
+		_putchar(48 + prod);
+	else if (prod > 9 && prod < 100)
+	{
+		_putchar(48 + f_d);
+		_putchar(48 + l_d);
+	}
+	else
+	{
+		_putchar(48 + f_d);
+		_putchar(48 + m_d);
+		_putchar(48 + l_d);
+	}
+}
+
+/**
+ * print_seperator - Prints the commas and space where
+ * neccesary
+ *
+ * @iter: Second iteration value
+ * @prod: Product of loop digit
+ *
+ * Return: void
+ */
+void print_seperator(int iter, int prod)
+{
+	if (iter != 0)
+	{
+		_putchar(44);
+		_putchar(32);
+
+		if (prod < 10)
+		{
+			_putchar(32);
+			_putchar(32);
+		}
+		else if (prod > 9 && prod < 100)
+			_putchar(32);
 	}
 }
