27d752c98341525a8549cc7e13d85bc508b9433b
Fortune <nachodev369@gmail.com>
2023-05-23 18:12:33 -0700

    Task 14. Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int iter, max = 50;
+	long int curr = 2, next;
+	long int sum = 1;
+
+	for (iter = 0; iter < 50; iter++)
+	{
+		if (sum != 1)
+			printf(", ");
+
+		printf("%ld", sum);
+
+		next = sum + curr;
+		sum = curr;
+		curr = next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
