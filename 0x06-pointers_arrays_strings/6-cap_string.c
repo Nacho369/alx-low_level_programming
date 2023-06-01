d0302c605f82ccf4be76e875538c3a18005540c2
Fortune <nachodev369@gmail.com>
2023-06-01 05:31:10 -0700

    Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,13 +17,13 @@ char *cap_string(char *str)
 	for (iter = 0; str[iter] != '\0'; iter++)
 	{
 		if ((str[iter] >= 97 && str[iter] <= 122) &&
-		str[iter - 1] == 32 || str[iter - 1] == 9 ||
+		(str[iter - 1] == 32 || str[iter - 1] == 9 ||
 		str[iter - 1] == 10 || str[iter - 1] == 44 ||
 		str[iter - 1] == 59 || str[iter - 1] == 46 ||
 		str[iter - 1] == 33 || str[iter - 1] == 63 ||
 		str[iter - 1] == 34 || str[iter - 1] == 40 ||
 		str[iter - 1] == 41 || str[iter - 1] == 123 ||
-		str[iter - 1] == 125)
+		str[iter - 1] == 125))
 			str[iter] = str[iter] - 32;
 	}
 
