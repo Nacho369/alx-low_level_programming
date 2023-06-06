c7f78ae578cb26fbb77285b448faed7f45002de0
Fortune <nachodev369@gmail.com>
2023-06-06 16:19:08 -0700

    Task 5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,5 +11,28 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int indx, indx2 = 0, inc = 0;
+
+	if (needle[indx2] == '\0')
+		return (haystack);
+
+	for (indx = 0; haystack[indx] != '\0'; indx++)
+	{
+		if (haystack[indx] == needle[indx2])
+		{
+			while (needle[indx2] != '\0' &&
+			haystack[indx] == needle[indx2])
+			{
+				indx++;
+				indx2++;
+				inc++;
+
+				if (needle[indx2] == '\0')
+					return (haystack + (indx - inc));
+			}
+		}
+	}
+
+	return (NULL);
 
 }
