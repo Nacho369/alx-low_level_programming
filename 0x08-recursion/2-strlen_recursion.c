069eddc966ade2e84a7a65cf982588646c839bc4
Fortune <nachodev369@gmail.com>
2023-06-07 16:12:10 -0700

    Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _strlen_recursion - Finds the lenght of a string
+ *
+ * @s: Pointer to the string
+ *
+ * Return: Lenght of the string
+ */
+int _strlen_recursion(char*s)
+{
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len = len + 1 + _strlen_recursion(s + 1);
+		return (len);
+	}
+
+	return (len);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+## This directory contains task for the 0x08. C - Recursion
