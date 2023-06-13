b1bca0947e05e22b9ed987cbec4584c25b97bccd
Fortune <nachodev369@gmail.com>
2023-06-12 21:19:56 -0700

    Task 5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,5 +13,38 @@
  */
 int main(int argc, char **argv)
 {
-	int iter;
+	int iter = 0, count = 0, value;
+	int reg[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	value = atoi(argv[1]);
+
+	if (value < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (;;)
+	{
+		if (value == 0)
+			break;
+
+		if (value >= reg[iter])
+		{
+			count += (value / reg[iter]);
+			value = value % reg[iter];
+		}
+
+		iter++;
+	}
+
+	printf("%d\n", count);
+
+	return (0);
 }
