aa3f43c89ab60adfc1fa27d482cc98de2599238e
Fortune <nachodev369@gmail.com>
2023-06-13 18:51:05 -0700

    Task 9. My primary goal of hacking was the intellectual curiosity, the seduction of adventure

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * strtow - A function that splits a string into words
+ *
+ * @str: String to split
+ *
+ * Return: A pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	int indx, len = 0;
+	char **arr_str = NULL;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	for (indx = 0; str[indx] != '\0'; indx++)
+	{
+		if (str[indx] != 32)
+			continue;
+	}
+
+	arr_str = (char *) malloc(sizeof(char *));
+
+	if (arr_str == NULL)
+		return (NULL);
+
+	for (indx = 0; )
+}
