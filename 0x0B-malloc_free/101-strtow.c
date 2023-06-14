de6d0879502df251a5a13f0404c4606cb43668af
Fortune <nachodev369@gmail.com>
2023-06-14 14:26:57 -0700

    Task 6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,20 +10,69 @@
  */
 char **strtow(char *str)
 {
-	int indx, len = 0;
+	int indx, len_ch,  indx2 = 0, indx3 = 0, size = 0;
+	int row, col = 0;
 	char **arr_str = NULL;
 
-	if (str == NULL || str == "")
+	if (str == NULL || *str == '\0')
 		return (NULL);
 
 	for (indx = 0; str[indx] != '\0'; indx++)
 	{
-		if (str[indx] != 32)
-			continue;
+		if ((str[indx] != 32) &&
+		 (str[indx + 1] == 32 || str[indx + 1] == '\0'))
+			size++; /* Gets the number of pointers to create. */
 	}
 
-	arr_str = (char *) malloc(sizeof(char *));
+	/* Allocate space for the size number of pointers plus one for NULL */
+	arr_str = (char **) malloc(size * sizeof(char *));
 
 	if (arr_str == NULL)
 		return (NULL);
+
+	for (row = 0; row < size; row++)
+	{
+		len_ch = 0;
+
+		/* Get the lenght of each word */
+		for (; str[indx2] != '\0'; indx2++)
+		{
+			if (str[indx2] != 32)
+				len_ch++;
+
+			if (str[indx2] != 32 &&
+			(str[indx2 + 1] == 32 || str[indx2 + 1] == '\0'))
+			{
+				indx2 += 1;
+				break;
+			}
+		}
+
+		/* Allocate space for each wors based on the lenght gotten */
+		arr_str[row] = (char *) malloc(len_ch * sizeof(char));
+
+		if (arr_str[row] == NULL)
+			return (NULL); /* Need to free, not complete */
+
+		/* Assign each word to a pointe		r */
+		for (; str[indx3] != '\0'; indx3++)
+		{
+			if (str[indx3] != 32)
+			{
+				arr_str[row][col] = str[indx3];
+				col++;
+			}
+
+			if (str[indx3] != 32 &&
+			(str[indx3 + 1] == 32 || str[indx3 + 1] == '\0'))
+			{
+				indx3 += 1;
+				break;
+			}
+		}
+
+		/* arr_str[row][col] = '\0'; */
+	}
+
+	return (arr_str);
 }
