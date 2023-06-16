704d3528aec22ee475b68a245c436581da5033d0
Fortune <nachodev369@gmail.com>
2023-06-16 12:52:31 -0700

    Task 2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,10 +16,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *arr_ptr = NULL;
 
 	if (nmemb == 0)
-		return (0);
+		return (NULL);
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 
 	ptr = malloc(size * nmemb);
 
