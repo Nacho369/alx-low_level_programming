9653b192da26801785e546479b07fa9d5dc3e97a
Fortune <nachodev369@gmail.com>
2023-06-19 16:10:32 -0700

    Task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - A new type
+ *
+ * @name: Name value given
+ * @age: Age value given
+ * @owner: Owner value given
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
