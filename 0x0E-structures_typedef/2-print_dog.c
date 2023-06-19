d3ce084b8bb97a8fdfd23a23e645c9281f2bfa0e
Fortune <nachodev369@gmail.com>
2023-06-19 16:51:40 -0700

    Task 3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,18 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+
+	if (d->age == NULL)
+		printf("Age: (nil)\n");
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+ 
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
