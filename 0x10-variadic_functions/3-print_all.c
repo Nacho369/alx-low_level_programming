80223d079f6cc9756841557e86168687afa88819
Fortune <nachodev369@gmail.com>
2023-06-22 21:59:21 -0700

    Task 3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,8 +10,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	int iter;
-	va_list args;
+	va_list a;
+	unsigned int j = 0, start = 0;
+	char *p;
 
-	
+	va_start(a, format);
+	while (format && format[j] != '\0')
+	{
+		switch (format[j])
+		{case 'c':
+			switch (start)
+			{case 1: printf(", "); }
+			start = 1;
+			printf("%c", va_arg(a, int));
+			break;
+		case 'i':
+			switch (start)
+			{case 1: printf(", "); }
+			start = 1;
+			printf("%i", va_arg(a, int));
+			break;
+		case 'f':
+			switch (start)
+			{case 1: printf(", "); }
+			start = 1;
+			printf("%f", va_arg(a, double));
+			break;
+		case 's':
+			switch (start)
+			{case 1: printf(", "); }
+			start = 1;
+			p = va_arg(a, char*);
+			if (p)
+			{ printf("%s", p);
+			break; }
+			printf("%p", p);
+			break; }
+		j++;
+	}
+	printf("\n");
+	va_end(a);
 }
