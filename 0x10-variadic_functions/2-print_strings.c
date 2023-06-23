15c92ac7077fe0ae9c9c26bccf4a19209b9b270a
Fortune <nachodev369@gmail.com>
2023-06-22 21:55:48 -0700

    Task 2 Update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,13 +23,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (str == NULL)
 			printf("(nil)");
-
-		printf("%s", str);
+		else
+			printf("%s", str);
 
 		if (iter != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
