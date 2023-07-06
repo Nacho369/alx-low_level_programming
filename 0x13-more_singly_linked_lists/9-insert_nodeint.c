f5ce33bbeac1722661bd97bcfb0d44445cf8ea07
Fortune <nachodev369@gmail.com>
2023-07-05 18:36:31 -0700

    Task 9 Check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newNode == NULL || head == NULL)
 		return (NULL);
 
+	if (*head == NULL)
+		return (NULL);
+
 	newNode->n = n;
 	newNode->next = NULL;
 	temp = *head;
