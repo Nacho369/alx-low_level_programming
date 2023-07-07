2c28c5012a674640f66fe60234bb700491c82e6f
Fortune <nachodev369@gmail.com>
2023-07-07 15:31:13 -0700

    Task 0 Check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,8 +14,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int res = 0;
 	int iter;
 
+	if (b == NULL)
+		return (0);
+
 	for (iter = 0; b[iter] != '\0'; iter++)
 	{
+		if (b[iter] != '0' && b[iter] != '1')
+			return (0);
+
 		res <<= 1;
 
 		if (b[iter] == '1')
