9a2c7969c5ff31284b1830b1d6737422c1832d0d
Fortune <nachodev369@gmail.com>
2023-07-10 16:24:34 -0700

    Task 2 Check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_APPEND | O_WRONLY);
+	file = open(filename, O_RDWR | O_APPEND);
 
 	if (file == -1)
 		return (-1);
