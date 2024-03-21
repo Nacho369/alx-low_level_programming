6f90b70a226ece6eb08c9762d2a4ab1886140a42
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-22 00:25:39 +0100

    Task 2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,24 +14,19 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *current = *head;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
+	new_node->next = *head;
 	new_node->prev = NULL;
 
 	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+		(*head)->prev = new_node;
 
-	new_node->next = current;
-	new_node->prev = NULL;
+
 	*head = new_node;
 
 	return (new_node);
