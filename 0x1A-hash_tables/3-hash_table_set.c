ef46bd1264678e70fdf9c520d38324dd12af3d30
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-14 18:44:13 +0100

    Task 3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[indx]->value = strdup(value);
 		return (1);
 	}
-	
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
