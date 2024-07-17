f940121d4083265e75da5645cc2712a9f6c97fa0
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-17 18:19:11 +0100

    Task 0. Linear search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,19 +9,19 @@ void print_array(int *array, size_t inc, size_t size);
 
 /* DATA STRUCTURES */
 /**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
+* struct listint_s - singly linked list
+*
+* @n: Integer
+* @index: Index of the node in the list
+* @next: Pointer to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -36,10 +36,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 /* FUNCTIONS PROTOTYPES */
